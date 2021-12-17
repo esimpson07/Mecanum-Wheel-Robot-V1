591de91489544638babbe731c676736d9cc48323
esimpson07 <76537760+esimpson07@users.noreply.github.com>
2021-12-16 20:00:45 -0500

    Add files via upload
    Jetson Nano human detection and x from center serial printing

---
+++ opencvnavigation.py
@@ -0,0 +1,73 @@
+import cv2 as cv
+import numpy as np
+import serial
+import time
+
+classes = ["background", "person", "bicycle", "car", "motorcycle",
+  "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+  "unknown", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+  "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "unknown", "backpack",
+  "umbrella", "unknown", "unknown", "handbag", "tie", "suitcase", "frisbee", "skis",
+  "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+  "surfboard", "tennis racket", "bottle", "unknown", "wine glass", "cup", "fork", "knife",
+  "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog",
+  "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "unknown", "dining table",
+  "unknown", "unknown", "toilet", "unknown", "tv", "laptop", "mouse", "remote", "keyboard",
+  "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "unknown",
+  "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush" ]
+
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+cam = cv.VideoCapture(0)
+pb  = 'frozen_inference_graph.pb'
+pbt = 'ssd_inception_v2_coco_2017_11_17.pbtxt'
+cvNet = cv.dnn.readNetFromTensorflow(pb,pbt)
+ser = serial.Serial('COM3', 1000000, timeout = 1)
+tolerance = 10
+xlength = 0
+xfromcenter = 0
+currentTime = time.time()
+pastTime = time.time()
+delay = 1
+
+ser.close()
+ser.open()
+
+while True:
+  currentTime = time.time()
+  ret_val, img = cam.read()
+  rows = img.shape[0]
+  cols = img.shape[1]
+  xlength = int(cols)
+  cvNet.setInput(cv.dnn.blobFromImage(img, size=(300,300), swapRB=True, crop=False))
+  cvOut = cvNet.forward()
+  for detection in cvOut[0,0,:,:]:
+    score = float(detection[2])
+    if score > 0.3:
+      idx = int(detection[1])
+      if classes[idx] == 'person':          
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+        midx = int((left + right) / 2)
+        midy = int((top + bottom) / 2)
+        if midx < ((xlength / 2) - tolerance):
+          xfromcenter = (xlength / 2) - midx
+        else:
+          if midx > ((xlength / 2) + tolerance):
+            xfromcenter = (xlength / 2) - midx
+          else:
+            xfromcenter = (xlength / 2) - midx
+        xfc = (str(xfromcenter))
+        if currentTime >= pastTime + delay:
+          pastTime = currentTime
+          ser.write(xfc.encode('utf-8'))
+        cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+        label = "{}: {:.2f}%".format(classes[idx],score * 100)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv.putText(img, label, (int(left), int(y)),cv.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)
+  cv.imshow('OpenCV Detection', img)                            
+  if cv.waitKey(1) == 27: 
+    break
+cam.release()
+cv.destroyAllWindows()
